b708b176d02f2942a57fc8f59e39d34fe956fe2d
Demetris Kyriacou <dk622@aube.doc.ic.ac.uk>
2023-02-24 22:26:40 +0000

    Change PrimaryMarket

--- src/contracts/PrimaryMarket.sol
+++ src/contracts/PrimaryMarket.sol
@@ -8,28 +8,44 @@ import "./TicketNFT.sol";
 import "./PurchaseToken.sol";
 
 contract PrimaryMarket is IPrimaryMarket {
-    address public ticketNFT;
-    address public owner;
+    address public primaryMarket;
     uint256 public ticketPrice;
-    uint256 public ticketID;
+    uint256 public issuedTicketNFTs;
     ITicketNFT public ticketNFTContract;
     IERC20 public purchaseTokenContract;
 
     constructor(address purchaseTokenContractAddress) {
         purchaseTokenContract = IERC20(purchaseTokenContractAddress);
         ticketNFTContract = new TicketNFT();
-        owner = msg.sender;
+        primaryMarket = msg.sender;
         ticketPrice = 100e18;
+        issuedTicketNFTs = 0;
     }
 
     function admin() external view override returns (address) {
-        return owner;
+        return primaryMarket;
     }
 
     function purchase(string memory holderName) external override {
-        ticketID++;
+        // check if msg.sender has enough purchaseToken
+        require(purchaseTokenContract.balanceOf(msg.sender) >= ticketPrice, "You do not have enough purchaseToken to purchase a ticket");
+        // check if msg.sender has approved primaryMarket to spend purchaseToken
+        require(purchaseTokenContract.allowance(msg.sender, primaryMarket) >= ticketPrice, "You have not approved primaryMarket to spend your purchaseToken");
+        // require(purchaseTokenContract.allowance(msg.sender, address(this)) >= ticketPrice, "You have not approved primaryMarket to spend your purchaseToken"
+        // check total number of issued tickets to be less than 1000
+        require(issuedTicketNFTs < 1000, "All tickets have been sold");
+        // transfers funds to owner from 
+        purchaseTokenContract.transferFrom(msg.sender, primaryMarket, ticketPrice);
+        issuedTicketNFTs++;
+        // mints ticketNFT to msg.sender
         ticketNFTContract.mint(msg.sender, holderName);
+        emit Purchase(msg.sender, holderName);
     }
+    // costs ticketPrice
+    // 
+
+
+
 
 
 }
--- test/PrimaryMarket.t.sol
+++ test/PrimaryMarket.t.sol
@@ -7,12 +7,18 @@ import "../src/contracts/PurchaseToken.sol";
 import "../src/contracts/TicketNFT.sol";
 
 contract PrimaryMarketTest is Test {
+    event Purchase(address indexed holder, string indexed holderName);
+
     PrimaryMarket public primaryMarket;
     PurchaseToken public purchaseToken;
     TicketNFT public ticketNFT;
     address public alice = makeAddr("alice");
     address public bob = makeAddr("bob");
     address public charlie = makeAddr("charlie");
+    // create a new account with 100 ether
+    // address alice = address(new Account(100 ether));
+    // add funds to Alice's account
+    // payable(alice).transfer(100 ether);
 
     function setUp() public {
         purchaseToken = new PurchaseToken();
@@ -22,6 +28,34 @@ contract PrimaryMarketTest is Test {
 
     function testAdmin() public {
         assertEq(primaryMarket.admin(), address(this));
+    }
+
+    function testPurchase() public {
+        // assertEq(primaryMarket.issuedTicketNFTs(), 0);
+        assertEq(purchaseToken.balanceOf(alice), 0);
+        assertEq(ticketNFT.balanceOf(alice), 0);
+        assertEq(purchaseToken.balanceOf(address(primaryMarket)), 0);
+
+        purchaseToken.mint{value: 10 ether}();
+        // assertGe(purchaseToken.balanceOf(alice), 1 ether);
+        // assertGe(purchaseToken.balanceOf(address(primaryMarket)), 1 ether);
+        assertEq(purchaseToken.balanceOf(address(this)), 1000e18 );
+        vm.prank(alice);
+        purchaseToken.mint{value: 10 ether}();
+        assertEq(purchaseToken.balanceOf(alice), 1000e18);
+
+        // assertEq(purchaseToken.balanceOf(alice), 10 ether);
+        // assertEq(purchaseToken.balanceOf(address(primaryMarket)), 10 ether);
+
+        // purchaseToken.approve(address(primaryMarket), 100e18);
+
+        // vm.expectEmit(true, true, false, true);
+        // emit Purchase(alice, "Alice");
+        // primaryMarket.purchase("Alice");
+        // // assertEq(primaryMarket.issuedTicketNFTs(), 1);
+        // assertEq(ticketNFT.balanceOf(alice), 1);
+        // // assertEq(purchaseToken.balanceOf(alice), 0);
+        // assertEq(purchaseToken.balanceOf(address(primaryMarket)), 100e18);
     }
 
 }
